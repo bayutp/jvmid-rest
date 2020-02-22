42c3ff470e378e0cc24f126c79d1bb74d954e052
Bayu <bayuteguhpamuji@gmail.com>
2020-02-22 14:46:33 +0700

    [bayu] setup local db

--- src/main/java/com/bayupamuji/restapi/domain/Car.java
+++ src/main/java/com/bayupamuji/restapi/domain/Car.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Car {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
     private String brand;
     private String model;
